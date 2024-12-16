a721d960beece06196410ee33150a0699d1882f1
Francsico Martins <kikofabcm@gmail.com>
2024-12-16 19:30:11 +0000

    .

--- teste2.py
+++ teste2.py
@@ -2,11 +2,11 @@ from stable_baselines3 import PPO
 from gymnasium.envs.registration import make
 import os
 
-models_dir = "models/PPO"
+models_dir = "models/PPO_"
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-env = make('CarRacing-v3', render_mode='human')
+env = make('CarRacing-v3')
 env.reset()
 
 model = PPO('CnnPolicy', env, verbose=1)
@@ -21,22 +21,3 @@ while iters < MAX_ITERATIONS:
     model.save(f"{models_dir}/ppo_car_racing_{TIMESTEPS * iters}")
 
 env.close()
-
-best_model_path = f"{models_dir}/ppo_car_racing_{TIMESTEPS * iters}"
-trained_model = PPO.load(best_model_path)
-
-env = make('CarRacing-v3', render_mode='human')
-obs, _ = env.reset()
-
-done = False
-total_reward = 0
-
-while not done:
-    action, _ = trained_model.predict(obs)
-    obs, reward, done, _, _ = env.step(action)
-    total_reward += reward
-    env.render()
-
-env.close()
-
-print(f"Showcase completed with total reward: {total_reward}")
